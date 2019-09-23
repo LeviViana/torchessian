0a62a88b36e9a51c4e9a8436c967f2de317d4a98
WinticsAI <levi.viana@wintics.com>
2019-09-23 15:11:46 +0200

    Ready to test on CUDA

--- torchessian/__init__.py
+++ torchessian/__init__.py
@@ -1,15 +1,20 @@
 import torch
 from copy import deepcopy
+import math
 
+SIG = 0.001
+EPS = 0.001
 
 def hessian_matmul(model, loss_function, v, batch):
-    r = 0.001
+    global EPS
+    
+    model.zero_grad()
     model_delta = deepcopy(model)
     begin = end = 0
     for p in model_delta.parameters():
         end = begin + p.data.numel()
         p_flat = p.data.view(-1)        
-        p_flat += r * v[begin:end]
+        p_flat += EPS * v[begin:end]
         begin = end
     
     x, y = batch
@@ -27,11 +32,10 @@ def hessian_matmul(model, loss_function, v, batch):
     
     model.zero_grad()
     
-    return (grad_w_delta - grad_w) / r
+    return (grad_w_delta - grad_w) / EPS
 
 
 def lanczos(model, loss_function, batch, m):
-    global H
     n = sum(p.data.numel() for p in model.parameters())
     v = torch.ones(n)
     v /= torch.norm(v)
@@ -79,3 +83,31 @@ def lanczos(model, loss_function, batch, m):
 
     V = torch.cat(list(v.unsqueeze(0) for v in V), 0)
     return T, V
+
+
+def gauss_quadrature(model, loss_function, batch, m):
+    T, _ = lanczos(model, loss_function, batch, m)
+    D, U = torch.eig(T, eigenvectors=True)
+    L = D[:, 0] # All eingenvalues are real anyway
+    W = torch.Tensor(list(U[0, i] ** 2 for i in range(m)))
+    return L, W
+
+
+def f(l, t, sig):
+    return torch.exp(-(t - l) ** 2 / (2 * sig)) / (sig * math.sqrt(2 * math.pi))
+
+
+def F(x, L, W, m):
+    global SIG
+    result = torch.zeros_like(x)
+    for i in range(m):
+        result += f(L[i], x, SIG) * W[i]
+    
+    return result
+
+
+def spectrum(model, loss_function, batch, m, x_min=-100, x_max=100):
+    L, W = gauss_quadrature(model, loss_function, batch, m)
+    support = torch.linspace(x_min, x_max, 1000)
+    density = F(support, L, W, m)
+    return support, density
